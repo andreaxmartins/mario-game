55fe383072bef46f7742aa7b5853f0b63dfd5318
Andrea Martins <andreaxmartins@gmail.com>
2022-07-19 22:59:22 +0200

    Create loop function

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const mario = document.querySelector(".mario");
+const pipe = document.querySelector(".pipe");
 
 const jump = () => {
   mario.classList.add("jump");
@@ -8,7 +9,17 @@ const jump = () => {
     mario.classList.remove("jump");
 
   }, 500);
-
 }
+
+const loop = setInterval(() => {
+
+  const pipePosition = pipe.offsetLeft;
+
+  if (pipePosition <= 124) {
+    pipe.style.animation = "none";
+    pipe.style.left = `${pipePosition}px`;
+  }
+
+}, 10);
 
 document.addEventListener("keydown", jump)
