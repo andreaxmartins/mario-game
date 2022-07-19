3058d7c29fd7608fab76d7ebcee310afdfa1f94d
Andrea Martins <andreaxmartins@gmail.com>
2022-07-19 23:35:11 +0200

    Add clearInterval to loop

--- js/script.js
+++ js/script.js
@@ -24,6 +24,8 @@ const loop = setInterval(() => {
     mario.src = "./images/game-over.png";
     mario.style.width = "75px";
     mario.style.marginLeft = "50px";
+
+    clearInterval(loop);
   }
 
 }, 10);
